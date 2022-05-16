2928feaae4e14fc345dd45f609a35a605fbbc80f
fleshka4 <sechko101@gmail.com>
2022-05-16 12:25:44 +0300

    Add handling exceptions and form to delete person

--- src/main/java/org/fleshka4/coursework/controller/ui/UIController.java
+++ src/main/java/org/fleshka4/coursework/controller/ui/UIController.java
@@ -4,23 +4,22 @@ import org.fleshka4.coursework.model.Diagnosis;
 import org.fleshka4.coursework.model.Person;
 import org.fleshka4.coursework.model.User;
 import org.fleshka4.coursework.model.Ward;
+import org.fleshka4.coursework.model.request.DelRequest;
 import org.fleshka4.coursework.model.request.DiagAndWardRequest;
 import org.fleshka4.coursework.repository.DiagnosisRepository;
 import org.fleshka4.coursework.repository.PersonRepository;
 import org.fleshka4.coursework.repository.UserRepository;
 import org.fleshka4.coursework.repository.WardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.server.ResponseStatusException;
 
-import javax.persistence.EntityNotFoundException;
 import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UIController {
@@ -100,19 +99,23 @@ public class UIController {
 
     @GetMapping("/diagnoses/{id}")
     public String showDiagnosis(@PathVariable("id") Integer id, final Model model) {
-        final Diagnosis diagnosis = diagnosisRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Diagnosis not found"));
-        model.addAttribute("diagnosis", diagnosis);
-        model.addAttribute("people", diagnosis.getPeople());
+        final Optional<Diagnosis> diagnosis = diagnosisRepository.findById(id);
+        if (diagnosis.isEmpty()) {
+            return "/blocks/not_found";
+        }
+        model.addAttribute("diagnosis", diagnosis.get());
+        model.addAttribute("people", diagnosis.get().getPeople());
         return "/diagnoses/diagnosis";
     }
 
     @GetMapping("/wards/{id}")
     public String showWard(@PathVariable("id") Integer id, final Model model) {
-        final Ward ward = wardRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Diagnosis not found"));
-        model.addAttribute("ward", ward);
-        model.addAttribute("people", ward.getPeople());
+        final Optional<Ward> ward = wardRepository.findById(id);
+        if (ward.isEmpty()) {
+            return "/blocks/not_found";
+        }
+        model.addAttribute("ward", ward.get());
+        model.addAttribute("people", ward.get().getPeople());
         return "/wards/ward";
     }
 
@@ -127,15 +130,23 @@ public class UIController {
 
     @PostMapping("/people/add")
     public String processPersonRegistration(final Person person, final DiagAndWardRequest diagAndWardRequest) {
-        final Diagnosis diagnosis = diagnosisRepository.findByName(diagAndWardRequest.getDiagnosisName())
-                .orElseThrow(() -> new EntityNotFoundException("Diagnosis not found"));
-        person.setDiagnosisId(diagnosis);
-        final Ward ward = wardRepository.findByName(diagAndWardRequest.getWardName())
-                .orElseThrow(() -> new EntityNotFoundException("Ward not found"));
-        if (ward.getMaxCount() <= ward.getPeople().size()) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Ward is full");
+        if (!personRepository.findPeopleByFullName(person.getFirstName(), person.getLastName(),
+                person.getMiddleName()).isEmpty()) {
+            return "/blocks/already_exists";
         }
-        person.setWardId(ward);
+        final Optional<Diagnosis> diagnosis = diagnosisRepository.findByName(diagAndWardRequest.getDiagnosisName());
+        if (diagnosis.isEmpty()) {
+            return "/blocks/not_found";
+        }
+        person.setDiagnosisId(diagnosis.get());
+        final Optional<Ward> ward = wardRepository.findByName(diagAndWardRequest.getWardName());
+        if (ward.isEmpty()) {
+            return "/blocks/not_found";
+        }
+        if (ward.get().getMaxCount() <= ward.get().getPeople().size()) {
+            return "wards/full";
+        }
+        person.setWardId(ward.get());
         personRepository.save(person);
         return "/people/people";
     }
@@ -148,6 +159,9 @@ public class UIController {
 
     @PostMapping("/diagnoses/add")
     public String processDiagnosisRegistration(final Diagnosis diagnosis) {
+        if (diagnosisRepository.existsByName(diagnosis.getName())) {
+            return "/blocks/already_exists";
+        }
         diagnosisRepository.save(diagnosis);
         return "/diagnoses/diagnoses";
     }
@@ -160,8 +174,27 @@ public class UIController {
 
     @PostMapping("/wards/add")
     public String processWardRegistration(final Ward ward) {
+        if (wardRepository.existsByName(ward.getName())) {
+            return "/blocks/already_exists";
+        }
         wardRepository.save(ward);
         return "/wards/wards";
+    }
+
+    @GetMapping("/people/delete_form")
+    public String showDeleteForm(final Model model) {
+        model.addAttribute("id", new DelRequest());
+        return "/people/delete_person";
+    }
+
+    @GetMapping("/people/delete")
+    public String processDeletePerson(final DelRequest delRequest) {
+        if (personRepository.existsById(delRequest.getId())) {
+            personRepository.deleteById(delRequest.getId());
+        } else {
+            return "/blocks/not_found";
+        }
+        return "/people/people";
     }
 
     public static boolean isNotNull(final Object obj) throws IllegalAccessException {
---
+++ src/main/java/org/fleshka4/coursework/model/request/DelRequest.java
@@ -0,0 +1,13 @@
+package org.fleshka4.coursework.model.request;
+
+public class DelRequest {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
